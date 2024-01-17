0adddcc5b93230efa00724dc10e287655531906d
Leonarddoo <samuel.pomin@gmail.com>
2024-01-17 19:01:10 +0100

    feat(auth): Setup Auth system

---
+++ codelab-auth/src/main/java/org/arobase/infrastructure/persistance/entity/UserCredential.java
@@ -0,0 +1,16 @@
+package org.arobase.infrastructure.persistance.entity;
+
+public class UserCredential {
+
+    private String username;
+
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
