c7d5b4cb06403ae8f0bbc199c491b19d865e53ce
BrachV <v.brach57@gmail.com>
2024-02-19 11:12:41 +0100

    feat(next/types): api+authStore types

---
+++ codelab-nextjs/utils/types/index.ts
@@ -0,0 +1,64 @@
+export type AuthState = {
+	profileId: string | null;
+	accessToken: string;
+	refreshToken: string;
+};
+export type AuthStore = {
+	profileId: string | null;
+	accessToken: string;
+	refreshToken: string;
+	setProfileId: (profileId: string) => void;
+	setAccessToken: (accessToken: string) => void;
+	setRefreshToken: (refreshToken: string) => void;
+	logout: () => void;
+};
+
+export type LoginBody = {
+	mail: string;
+	password: string;
+};
+export type LoginResponse = {
+	profileId: string;
+	accessToken: string;
+	refreshToken: string;
+};
+export type RegisterBody = {
+	mail: string;
+	username: string;
+	password: string;
+	confirmPassword: string;
+};
+export type RegisterResponse = {
+	profileId: string;
+	accessToken: string;
+	refreshToken: string;
+};
+
+export type SubmitExerciseBody = {
+	language: "javascript" | "python" | "java" | "csharp"; 
+	testCode: string;
+	author: string;
+};
+export type SubmitExerciseResponse = {
+	id: string;
+	feedback: string;
+};
+
+export type CreateExerciseBody = {
+	language: "javascript" | "python" | "java" | "csharp";
+	testCode: string;
+};
+export type CreateExerciseResponse = {
+	language: string;
+	testCode: string;
+	author: string;
+};
+
+export type GetResultsExerciceResponse = {
+	id: string;
+	status: "PENDING"|"ERROR"|"COMPLETED";
+	result: string;
+	timestamp: string;
+	errorDetails: string|null;
+	additionalInfo: string|null;
+};
