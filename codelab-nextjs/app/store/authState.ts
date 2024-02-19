7a21e8f0b148cdceea9167702aee41a3641bc7c9
BrachV <v.brach57@gmail.com>
2024-02-19 14:10:28 +0100

    refactor(next/authStore): now using type AuthStore only on persist method

--- codelab-nextjs/app/store/authState.ts
+++ codelab-nextjs/app/store/authState.ts
@@ -1,28 +1,23 @@
-import { AuthState, AuthStore } from "@/utils/types";
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
-
+import { AuthStore } from '@/utils/types';
+import {create} from 'zustand';
+import { persist } from 'zustand/middleware';
 
 export const useAuthStore = create(
-  persist(
-    (set) => ({
-      profileId: null,
-      accessToken: "",
-      refreshToken: "",
+	persist<AuthStore>(
+		(set) => ({
+			// État initial
+			profileId: null,
+			accessToken: "",
+			refreshToken: "",
 
-      setProfileId: (profileId: string) => set({ profileId }),
-      setAccessToken: (accessToken: string) => set({ accessToken }),
-      setRefreshToken: (refreshToken: string) => set({ refreshToken }),
-
-      logout: () =>
-        set({
-          profileId: null,
-          accessToken: "",
-          refreshToken: "",
-        } as AuthState),
-    } as AuthStore),
-    {
-      name: "auth",
-    }
-  )
+			// Actions pour modifier l'état
+			setProfileId: (profileId) => set({ profileId }),
+			setAccessToken: (accessToken) => set({ accessToken }),
+			setRefreshToken: (refreshToken) => set({ refreshToken }),
+			logout: () => set({ profileId: null, accessToken: "", refreshToken: "" }),
+		}),
+		{
+			name: "auth", // Nom de la clé sous laquelle l'état persistant est stocké dans le localStorage
+		}
+	)
 );
