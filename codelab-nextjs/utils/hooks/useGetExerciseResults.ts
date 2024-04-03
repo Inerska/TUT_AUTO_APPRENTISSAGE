1e98700aaf628c3636d1d140110b616a7fd8c028
BrachV <v.brach57@gmail.com>
2024-04-03 23:29:16 +0200

    Refactor useGetExerciseResults hook

--- codelab-nextjs/utils/hooks/useGetExerciseResults.ts
+++ codelab-nextjs/utils/hooks/useGetExerciseResults.ts
@@ -1,35 +1,35 @@
-import {useEffect, useState} from "react";
-import {getExerciseResults} from "@/service/apiServiceExercise";
-import {GetResultsExerciceResponse} from "@/utils/types";
+import { useEffect, useState } from "react";
+import { getExerciseResults } from "@/service/apiServiceExercise";
+import { Exercise } from "@/utils/types";
 
-export function useGetExerciseResults(code: string, submitLoad: boolean,exerciseId? : string) {
+export function useGetExerciseResults(code: string, submitLoad: boolean, exerciseId?: string) {
 
-    const [results, setResults] = useState<GetResultsExerciceResponse>();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState();
-    const [pending, setPending] = useState<boolean>(true)
+	const [results, setResults] = useState<Exercise>();
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState();
+	const [pending, setPending] = useState<boolean>(true)
 
-    const [refresh, refreshData] = useState();
+	const [refresh, refreshData] = useState();
 
-    useEffect(() => {
-        if(code !== "" && !submitLoad && exerciseId) {
-            setLoading(true);
-            getExerciseResults(exerciseId)
-                .then(data => {
-                    console.log(data);
-                    if (data.status != "PENDING") {
-                        setPending(false);
-                    }
-                    setResults(data)
-                })
-                .catch((err) => {
-                    setError(err);
-                })
-                .finally(() => {
-                    setLoading(false);
-                })
-        }
-    }, [code, exerciseId, refresh, submitLoad] )
+	useEffect(() => {
+		if (code !== "" && !submitLoad && exerciseId) {
+			setLoading(true);
+			getExerciseResults(exerciseId)
+				.then(data => {
+					console.log(data);
+					if (data.status != "PENDING") {
+						setPending(false);
+					}
+					setResults(data)
+				})
+				.catch((err) => {
+					setError(err);
+				})
+				.finally(() => {
+					setLoading(false);
+				})
+		}
+	}, [code, exerciseId, refresh, submitLoad])
 
-    return { results, pending, loading, error, refreshData };
+	return { results, pending, loading, error, refreshData };
 }
