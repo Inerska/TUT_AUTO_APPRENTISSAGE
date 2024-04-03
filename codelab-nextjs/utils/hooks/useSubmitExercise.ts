1c1a66a33a3aca7949f2380b81b0dedfecb94fef
BrachV <v.brach57@gmail.com>
2024-04-03 23:29:13 +0200

    Refactor useSubmitExercise hook

--- codelab-nextjs/utils/hooks/useSubmitExercise.ts
+++ codelab-nextjs/utils/hooks/useSubmitExercise.ts
@@ -1,32 +1,32 @@
-import {useEffect, useState} from "react";
-import { submitExercise} from "@/service/apiServiceExercise";
-import {SubmitExerciseBody, SubmitExerciseResponse} from "@/utils/types";
+import { useEffect, useState } from "react";
+import { submitExercise } from "@/service/apiServiceExercise";
+import { SubmitExerciseBody, SubmitExerciseResponse } from "@/utils/types";
 
-export function useSubmitExercise(exercise : SubmitExerciseBody) {
+export function useSubmitExercise(exercise: SubmitExerciseBody) {
 
-    const [response, setResponse] = useState<SubmitExerciseResponse>();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState();
+	const [response, setResponse] = useState<SubmitExerciseResponse>();
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState();
 
-    const [isExerciseSubmitted, setIsExerciseSubmitted] = useState(false);
+	const [isExerciseSubmitted, setIsExerciseSubmitted] = useState(false);
 
-    useEffect(() => {
-            if(exercise.code !== "" && !isExerciseSubmitted) {
-                setLoading(true);
-                setIsExerciseSubmitted(true);
-                submitExercise(exercise)
-                    .then(data => {
-                        console.log(data);
-                        setResponse(data)
-                    })
-                    .catch((err) => {
-                        setError(err);
-                    })
-                    .finally(() => {
-                        setLoading(false);
-                    })
-            }
-    }, [exercise, isExerciseSubmitted], )
+	useEffect(() => {
+		if (exercise.code !== "" && !isExerciseSubmitted) {
+			setLoading(true);
+			setIsExerciseSubmitted(true);
+			submitExercise(exercise)
+				.then(data => {
+					console.log(data);
+					setResponse(data)
+				})
+				.catch((err) => {
+					setError(err);
+				})
+				.finally(() => {
+					setLoading(false);
+				})
+		}
+	}, [exercise, isExerciseSubmitted],)
 
-    return { response, loading, error, setIsExerciseSubmitted };
+	return { response, loading, error, setIsExerciseSubmitted };
 }
