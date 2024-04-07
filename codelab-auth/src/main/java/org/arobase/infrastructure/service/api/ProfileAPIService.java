304f3cc6969b8ac29a29f385610f56cd38c9aeac
Maxime48 <maxime48@users.noreply.github.com>
2024-04-07 18:15:22 +0200

    AUTH -> EXERCISE profile on register | Commit2

--- codelab-auth/src/main/java/org/arobase/infrastructure/service/api/ProfileAPIService.java
+++ codelab-auth/src/main/java/org/arobase/infrastructure/service/api/ProfileAPIService.java
@@ -1,18 +1,48 @@
 package org.arobase.infrastructure.service.api;
 
-import io.smallrye.mutiny.Uni;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import io.vertx.uritemplate.impl.UriTemplateImpl;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import org.jboss.logging.Logger;
 
-@RegisterRestClient(baseUri = "http://exercices.codelab.local:8080/api/v1")
-@Path("/profile/65d4a80560ce2b7f8766cad8")
-public interface ProfileAPIService {
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
-    @GET
-    @Path("profile/65d4ce440cd10a2bd0afa4c3")
-    @Produces(jakarta.ws.rs.core.MediaType.APPLICATION_JSON)
-    Uni<String> getProfile();
+@ApplicationScoped
+public class ProfileAPIService {
+
+    private final String apiUrl = "http://exercices.codelab.local:8080/api/v1/profile";
+
+    @Inject
+    Logger logger;
+
+    public String createProfile(String username) throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(apiUrl))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(
+                    Json.createObjectBuilder().add("username", username).build().toString()
+                ))
+                .build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            logger.info("Response from create profile: " + response.body());
+            JsonObject jsonObject = Json.createReader(new StringReader(response.body())).readObject();
+            return jsonObject.getString("profile-id");
+        }catch (Exception e){
+            throw new RuntimeException("Error while creating profile" + e);
+        }
+
+    }
 
 }
