da21ccad2cf6d2d9570f6946848902dbba03ea85
tpellizzari <theo.pellizzari2@ent.univ-lorraine.fr>
2024-04-08 16:17:14 +0200

    add tests for exercices API

---
+++ codelab-exercices/src/test/java/DockerExecutionContextTest.java
@@ -0,0 +1,48 @@
+import com.github.dockerjava.api.DockerClient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+import org.arobase.domain.docker.service.DockerExecutionContext;
+
+/**
+ * Classe de test pour la classe DockerExecutionContext.
+ */
+public class DockerExecutionContextTest {
+
+    // Mock de DockerClient
+    private DockerClient dockerClientMock;
+    // Identifiant du conteneur
+    private final String containerId = "testContainerId";
+    // Instance de DockerExecutionContext à tester
+    private DockerExecutionContext dockerExecutionContext;
+
+    /**
+     * Méthode exécutée avant chaque test.
+     * Initialise les mocks et les instances nécessaires.
+     */
+    @BeforeEach
+    public void setUp() {
+        dockerClientMock = mock(DockerClient.class);
+        dockerExecutionContext = new DockerExecutionContext(dockerClientMock, containerId);
+    }
+
+    /**
+     * Teste la méthode getDockerClient.
+     * Vérifie si le DockerClient retourné est celui qui a été passé lors de la création de l'instance.
+     */
+    @Test
+    public void testGetDockerClient() {
+        assertEquals(dockerClientMock, dockerExecutionContext.getDockerClient());
+    }
+
+    /**
+     * Teste la méthode getContainerId.
+     * Vérifie si l'identifiant du conteneur retourné est celui qui a été passé lors de la création de l'instance.
+     */
+    @Test
+    public void testGetContainerId() {
+        assertEquals(containerId, dockerExecutionContext.getContainerId());
+    }
+}
